3ad06544d1dd95d25bf92544ee9492775013dbf5
Unai Motriko <unaimotriko@gmail.com>
2017-05-11 16:06:47 +0100

    It add css styling

--- mecha.js
+++ mecha.js
@@ -4,7 +4,18 @@ $(document).ready(function(){
   var th = new Thermostat;
   $('#temp').text(th._temp);
   $('#increase').on('click', function(){
-  th.up(1)
-  $('#temp').text(th._temp)
-})
+    th.up(1)
+    $('#temp').text(th._temp)
+  })
+  $('#decrease').on('click', function(){
+    th.down(1)
+    $('#temp').text(th._temp)
+  })
+  $('#on').on('click', function(){
+    th.powerModeOn()
+
+  })
+  $('#off').on('click', function(){
+    th.powerModeOff()
+  })
 });
