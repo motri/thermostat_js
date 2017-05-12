0a2a869fe4529aca4ab9d72b1caa7d91f5c6c931
Unai Motriko <unaimotriko@gmail.com>
2017-05-12 18:03:26 +0100

    It adds css styling

--- mecha.js
+++ mecha.js
@@ -8,34 +8,39 @@ $(document).ready(function(){
   var apiKey = '&appid=abc3302e9ff41d114253dcfe743b7ce4' ;
   var units = '&units=metric';
   var url = api + city + units + apiKey;
-  th.weather = $.get(url, function(weather){
-    $('#tempUpdate').text(weather['main'].temp + " C.");
+  th.weather = $.get(api + city + units + apiKey, function(weather){
+    $('#tempUpdate').text(weather['main'].temp + " °C");
     $('#weatherIcon').html("<img src='http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png' alt='Icon depicting current weather.'>")
   });
-
+  $('#submit').on('click', function(){
+    city = $('.city').select()
+    console.log(city)
+    th.weather
+    console.log(url)
+  });
   $('#temp').text(th._temp);
   $('#increase').on('click', function(){
     th.up(1);
-    $('#temp').text(th._temp);
     var temp = th._temp;
       if (temp < 18) {
-        $('#temp').css('color', 'green')
+        $('#temp').css('color', '#0c992d')
       }  else if (temp < 25) {
         $('#temp').css('color', 'black')
       } else {
-        $('#temp').css('color', 'red')
+        $('#temp').css('color', '#990c0c')
       }
+    $('#temp').text(th._temp);
   });
   $('#decrease').on('click', function(){
     th.down(1);
     $('#temp').text(th._temp);
     var temp = th._temp;
       if (temp < 18) {
-        $('#temp').css('color', 'green')
+        $('#temp').css('color', '#0c992d')
       }  else if (temp < 25) {
         $('#temp').css('color', 'black')
       } else {
-        $('#temp').css('color', 'red')
+        $('#temp').css('color', '#990c0c')
       }
   });
   $('#on').on('click', function(){
@@ -45,10 +50,9 @@ $(document).ready(function(){
   })
   $('#off').on('click', function(){
     th.powerModeOff();
-    $('#temp').text(th._temp);
     $("#powerOff").fadeTo(0, 1);
     $("#powerOn").fadeTo(0, 0);
-    $('#temp').text(th._temp);
+
   });
   $('#reset').on('click', function(){
     th.resetTemp();
